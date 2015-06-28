11b5327c43cd785b8522e9e10ea72203f9899eb1
Luis Alves <ljalvs@gmail.com>
2015-06-28 03:06:28 +0100

    widgets/flight_mode: Cleanup and add config
    Config mode allows to select font size.

--- firmware/alce-osd.X/widgets/flight_mode.c
+++ firmware/alce-osd.X/widgets/flight_mode.c
@@ -1,30 +1,28 @@
-#include <stdio.h>
-#include <string.h>
-#include <p33Exxxx.h>
+/*
+    AlceOSD - Graphical OSD
+    Copyright (C) 2015  Luis Alves
 
-#include "mavlink.h"
-#include "graphics.h"
-#include "widgets.h"
-#include "config.h"
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-#ifdef OSD_SMALL
-#define X_SIZE  170
-#define Y_SIZE  17
-#define X_POS   0
-#define Y_POS   OSD_YSIZE - 32 - 28
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
-#else
-#define X_SIZE  120
-#define Y_SIZE  7
-
-
-#endif
+#include "alce-osd.h"
 
 extern struct alceosd_config config;
 
 static struct widget_priv {
     unsigned int custom_mode, prev_custom_mode;
+    unsigned char font_id;
     struct canvas ca;
 } priv;
 
@@ -44,9 +42,20 @@ static void mav_callback(mavlink_message_t *msg, mavlink_status_t *status)
 static void init(struct widget_config *wcfg)
 {
     struct canvas *ca = &priv.ca;
+    unsigned int xs = 120, ys;
+    unsigned char m = wcfg->props.mode;
+    const struct font *f;
+
+    if (m > 2)
+        m = 2;
+
+    priv.font_id = m;
+
+    f = get_font(m);
+    ys = f->height + 1;
 
     priv.prev_custom_mode = 0xff;
-    alloc_canvas(ca, wcfg, X_SIZE, Y_SIZE);
+    alloc_canvas(ca, wcfg, xs, ys);
     add_mavlink_callback(MAVLINK_MSG_ID_HEARTBEAT, mav_callback, CALLBACK_WIDGET);
 }
 
@@ -66,86 +75,82 @@ static int render(void)
 
     switch (cust_mode) {
     case PLANE_MODE_MANUAL:
-        strcpy(mode, "manual");
+        strcpy(mode, "Manual");
         break;
     case PLANE_MODE_CIRCLE:
     case COPTER_MODE_CIRCLE:
-        strcpy(mode, "circle");
+        strcpy(mode, "Circle");
         break;
     case PLANE_MODE_STABILIZE:
     case COPTER_MODE_STABILIZE:
-        strcpy(mode, "stabilize");
+        strcpy(mode, "Stabilize");
         break;
     case PLANE_MODE_TRAINING:
-        strcpy(mode, "training");
+        strcpy(mode, "Training");
         break;
     case PLANE_MODE_ACRO:
     case COPTER_MODE_ACRO:
-        strcpy(mode, "acro");
+        strcpy(mode, "Acro");
         break;
     case PLANE_MODE_FBWA:
-        strcpy(mode, "fly-by-wire a");
+        strcpy(mode, "Fly-By-Wire A");
         break;
     case PLANE_MODE_FBWB:
-        strcpy(mode, "fly-by-wire b");
+        strcpy(mode, "Fly-By-Wire B");
         break;
     case PLANE_MODE_CRUISE:
-        strcpy(mode, "cruise");
+        strcpy(mode, "Cruise");
         break;
     case PLANE_MODE_AUTOTUNE:
     case COPTER_MODE_AUTOTUNE:
-        strcpy(mode, "auto tune");
+        strcpy(mode, "Auto tune");
         break;
     case PLANE_MODE_AUTO:
     case COPTER_MODE_AUTO:
-        strcpy(mode, "auto");
+        strcpy(mode, "Auto");
         break;
     case PLANE_MODE_RTL:
     case COPTER_MODE_RTL:
-        strcpy(mode, "return to launch");
+        strcpy(mode, "Return To Launch");
         break;
     case PLANE_MODE_LOITER:
     case COPTER_MODE_LOITER:
-        strcpy(mode, "loiter");
+        strcpy(mode, "Loiter");
         break;
     case PLANE_MODE_INIT:
-        strcpy(mode, "initializing");
+        strcpy(mode, "Initializing");
         break;
     case PLANE_MODE_GUIDED:
     case COPTER_MODE_GUIDED:
-        strcpy(mode, "guided");
+        strcpy(mode, "Guided");
         break;
     case COPTER_MODE_ALTHOLD:
-        strcpy(mode, "altitude hold");
+        strcpy(mode, "Altitude hold");
         break;
     case COPTER_MODE_LAND:
-        strcpy(mode, "land");
+        strcpy(mode, "Land");
         break;
     case COPTER_MODE_OF_LOITER:
-        strcpy(mode, "of loiter");
+        strcpy(mode, "OF Loiter");
         break;
     case COPTER_MODE_DRIFT:
-        strcpy(mode, "drift");
+        strcpy(mode, "Drift");
         break;
     case COPTER_MODE_SPORT:
-        strcpy(mode, "sport");
+        strcpy(mode, "Sport");
         break;
     case COPTER_MODE_FLIP:
-        strcpy(mode, "flip");
+        strcpy(mode, "Flip");
         break;
     case COPTER_MODE_POSHOLD:
-        strcpy(mode, "position hold");
+        strcpy(mode, "Position hold");
         break;
     default:
-        strcpy(mode, "unknown mode");
+        strcpy(mode, "Unknown Mode");
         break;
     }
 
-#ifdef OSD_SMALL
-    draw_str3(mode, 0, 0, ca);
-#else
-    draw_str(mode, 0, 0, ca, 0);
-#endif
+    draw_str(mode, 0, 0, ca, priv.font_id);
 
     schedule_canvas(ca);
     return 0;
