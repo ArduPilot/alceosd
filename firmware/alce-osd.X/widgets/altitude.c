fd3dc8f94e4c2ce5e47855222d9ca61bf1b5b2d5
Luis Alves <ljalvs@gmail.com>
2015-06-30 00:28:37 +0100

    widgets/altitude: Add range, units and mode
    Also rename widget as it can be setup to display home altitude also.

--- firmware/alce-osd.X/widgets/altitude.c
+++ firmware/alce-osd.X/widgets/altitude.c
@@ -20,27 +20,41 @@
 
 #define X_SIZE  48
 #define Y_SIZE  100
-
-/* range in degrees */
-#define RANGE       100
-#define MAJOR_TICK  20
-#define MINOR_TICK  5
 #define X_CENTER    (X_SIZE/2) - 15
 #define Y_CENTER    (Y_SIZE/2) - 1
 
 
 static struct widget_priv {
-    float altitude;
-    int altitude_i;
+    long altitude;
+    int range;
     struct canvas ca;
+    struct widget_config *cfg;
+    struct home_data *home;
 } priv;
 
 const struct widget altitude_widget;
 
 static void mav_callback(mavlink_message_t *msg, mavlink_status_t *status)
 {
-    priv.altitude = mavlink_msg_gps_raw_int_get_alt(msg) / 1000.0;
-    priv.altitude_i = (int) priv.altitude;
+    float altitude;
+    
+    switch (priv.cfg->props.mode) {
+        case 0:
+        default:
+            altitude = mavlink_msg_gps_raw_int_get_alt(msg) / 1000.0;
+            break;
+        case 1:
+            if (priv.home->lock == HOME_LOCKED)
+                altitude = (long) priv.home->altitude;
+            else
+                altitude = 0;
+            break;
+    }
+
+    if (priv.cfg->props.units == UNITS_IMPERIAL)
+        altitude *= M2FEET;
+    
+    priv.altitude = (long) altitude;
 
     schedule_widget(&altitude_widget);
 }
@@ -50,6 +64,19 @@ static void init(struct widget_config *wcfg)
 {
     struct canvas *ca = &priv.ca;
 
+    priv.cfg = wcfg;
+    priv.home = get_home_data();
+
+    switch (wcfg->props.units) {
+        case 0:
+        default:
+            priv.range = 20*5;
+            break;
+        case 1:
+            priv.range = 100*5;
+            break;
+    }
+
     add_mavlink_callback(MAVLINK_MSG_ID_GPS_RAW_INT, mav_callback, CALLBACK_WIDGET);
     alloc_canvas(ca, wcfg, X_SIZE, Y_SIZE);
 }
@@ -58,45 +85,53 @@ static void init(struct widget_config *wcfg)
 static int render(void)
 {
     struct canvas *ca = &priv.ca;
-    int i, j, y;
-    char buf[10];
+    int i, j, y = -1;
+    long yy;
+    char buf[10], d = 0;
+    int major_tick = priv.range / 5;
+    int minor_tick = major_tick / 4;
 
     if (init_canvas(ca, 0))
         return 1;
 
-
-    for(i = -RANGE / 2; i < RANGE / 2; i++) {
-        y = Y_CENTER - i;
-        j = priv.altitude_i + i;
-        if(j % MAJOR_TICK == 0) {
+    for (i = 0; i < priv.range; i++) {
+        yy = ((long) i * Y_SIZE) / priv.range;
+        if ((yy == y) && (d == 1))
+            continue;
+        y = Y_SIZE - (int) yy;
+        j = priv.altitude + i - priv.range/2;
+        if (j % major_tick == 0) {
             draw_ohline(X_CENTER + 2, X_CENTER - 4, y, 1, 3, ca);
             sprintf(buf, "%4d", j);
-            draw_str(buf, X_CENTER + 10, y-3, ca, 0);
-        } else if(j % MINOR_TICK == 0) {
+            draw_str(buf, X_CENTER + 13, y-3, ca, 0);
+            d = 1;
+        } else if (j % minor_tick == 0) {
             draw_ohline(X_CENTER + 2, X_CENTER - 2, y, 1, 3, ca);
+            d = 1;
+        } else {
+            d = 0;
         }
     }
 
-    draw_frect(X_CENTER + 10, Y_CENTER-3, X_SIZE-2, Y_CENTER + 3, 0, ca);
-    sprintf(buf, "%4d", priv.altitude_i);
-    draw_str(buf, X_CENTER + 10, Y_CENTER - 2, ca, 0);
+    draw_frect(X_CENTER + 10, Y_CENTER-4, X_SIZE-2, Y_CENTER + 4, 0, ca);
+    sprintf(buf, "%4ld", priv.altitude);
+    draw_str(buf, X_CENTER + 13, Y_CENTER - 3, ca, 0);
 
-    draw_hline(X_CENTER + 10, X_SIZE - 1, Y_CENTER-4, 1, ca);
-    draw_hline(X_CENTER + 10, X_SIZE - 1, Y_CENTER+4, 1, ca);
-    draw_vline(X_SIZE - 1, Y_CENTER-3, Y_CENTER+3, 1, ca);
+    draw_hline(X_CENTER + 10, X_SIZE - 1, Y_CENTER-5, 1, ca);
+    draw_hline(X_CENTER + 10, X_SIZE - 1, Y_CENTER+5, 1, ca);
+    draw_vline(X_SIZE - 1, Y_CENTER-4, Y_CENTER+4, 1, ca);
 
     /* draw arrow */
-    draw_line(X_CENTER+10, Y_CENTER-4, X_CENTER+10-4, Y_CENTER, 1, ca);
-    draw_line(X_CENTER+10, Y_CENTER+4, X_CENTER+10-4, Y_CENTER, 1, ca);
+    draw_line(X_CENTER+10, Y_CENTER-5, X_CENTER+10-5, Y_CENTER, 1, ca);
+    draw_line(X_CENTER+10, Y_CENTER+5, X_CENTER+10-5, Y_CENTER, 1, ca);
 
-    schedule_canvas(ca); //render_canvas(ca);
-
+    schedule_canvas(ca);
     return 0;
 }
 
 
 const struct widget altitude_widget = {
-    .name = "Altitude MSL",
+    .name = "Altitude (instrument)",
     .id = WIDGET_ALTITUDE_ID,
     .init = init,
     .render = render,
