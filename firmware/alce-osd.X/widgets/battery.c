0ce7fe4ce252bef978176fc4fb5b8c6d67dc65e2
Luis Alves <ljalvs@gmail.com>
2015-06-30 23:20:29 +0100

    widgets/battery: Cleanup and increase vertical bar size

--- firmware/alce-osd.X/widgets/battery.c
+++ firmware/alce-osd.X/widgets/battery.c
@@ -1,12 +1,22 @@
-#include <stdio.h>
-#include <math.h>
-#include <string.h>
-#include <p33Exxxx.h>
+/*
+    AlceOSD - Graphical OSD
+    Copyright (C) 2015  Luis Alves
 
-#include "mavlink.h"
-#include "graphics.h"
-#include "widgets.h"
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "alce-osd.h"
 
 #define X_SIZE  64
 #define Y_SIZE  45
@@ -15,7 +25,7 @@
 #define BAT_BAR_X   3
 #define BAT_BAR_Y   0
 #define BAT_BAR_W   58
-#define BAT_BAR_H   10
+#define BAT_BAR_H   11
 
 
 
