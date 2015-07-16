fcc1f7b43204d3a014f93cedd2134ec3cc5098bf
Luis Alves <ljalvs@gmail.com>
2015-07-16 18:42:31 +0100

    widgets/gps: Remove unused sat icon

--- firmware/alce-osd.X/widgets/gps.c
+++ firmware/alce-osd.X/widgets/gps.c
@@ -22,39 +22,9 @@
 #define X_SIZE  60 + 32 + 20 + 32
 #define Y_SIZE  18
 
-#define SAT_X   60
-#define SAT_Y   5
-
-#ifdef GPS_SAT_ICON
-const char sat_ico[] = {
-    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-    0,0,0,0,0,3,3,0,3,3,3,3,3,3,3,0,0,0,0,0,
-    0,0,0,0,3,3,3,3,1,1,1,1,1,1,3,3,0,0,0,0,
-    0,0,0,0,3,3,3,3,3,3,3,3,3,3,1,3,3,0,0,0,
-    0,0,0,0,3,3,3,3,1,1,1,1,1,1,1,3,3,3,0,0,
-    0,0,0,3,1,3,1,3,3,1,1,1,1,1,1,1,3,3,0,0,
-    0,0,3,1,1,3,1,1,3,3,1,1,1,1,1,3,3,3,0,0,
-    0,3,1,1,1,3,1,1,1,3,3,1,1,1,1,3,3,3,0,0,
-    0,3,1,1,1,3,1,1,1,1,3,3,1,1,1,3,3,3,0,0,
-    0,3,1,1,1,3,1,1,1,1,1,3,1,1,3,3,3,3,0,0,
-    0,3,1,1,1,3,1,1,1,1,1,1,1,3,3,3,3,3,0,0,
-    0,3,3,1,1,1,1,1,1,1,1,3,3,3,3,3,3,0,0,0,
-    0,3,3,3,1,1,1,1,1,1,3,3,3,3,3,3,3,0,0,0,
-    0,0,3,3,3,3,3,3,3,3,3,3,3,3,3,3,0,0,0,0,
-    0,0,0,3,3,3,3,3,3,3,3,3,3,3,3,0,0,0,0,0,
-    0,0,0,0,3,3,3,3,3,3,3,3,3,3,0,3,0,0,0,0,
-    0,0,0,0,0,0,3,3,3,3,3,0,0,0,3,3,0,0,0,0,
-    0,0,0,0,0,0,0,0,0,0,0,3,3,3,3,3,3,0,0,0,
-    0,0,0,0,0,0,0,0,0,3,3,3,3,3,3,3,3,3,0,0,
-    0,0,0,0,0,0,0,0,3,3,3,3,3,3,3,3,3,3,0,0,
-    0,0,0,0,0,0,0,0,3,3,3,3,3,3,3,3,3,3,3,0,
-    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-};
-#endif
 
 struct widget_priv {
     float gps_lat, gps_lon, gps_eph;
-    unsigned int gps_cog;
     unsigned char gps_nrsats, gps_fix_type;
 };
 
@@ -67,7 +37,6 @@ static void mav_callback(mavlink_message_t *msg, mavlink_status_t *status, void 
     priv->gps_lon = mavlink_msg_gps_raw_int_get_lon(msg) / 10000000.0;
     priv->gps_fix_type = mavlink_msg_gps_raw_int_get_fix_type(msg);
     priv->gps_nrsats = mavlink_msg_gps_raw_int_get_satellites_visible(msg);
-    //priv->gps_cog = mavlink_msg_gps_raw_int_get_cog(msg);
     priv->gps_eph = (float) mavlink_msg_gps_raw_int_get_eph(msg) / 100.0;
 
     schedule_widget(w);
@@ -91,21 +60,15 @@ static int init(struct widget *w)
 
 static void render(struct widget *w)
 {
-    struct widget_priv *priv = (struct widget_priv*) w->priv;
+    struct widget_priv *priv = w->priv;
     struct canvas *ca = &w->ca;
-    char buf[20], i, j;
+    char buf[20];
 
     sprintf(buf, "%10.6f", priv->gps_lat);
     draw_str(buf, 0, 0, ca, 0);
 
     sprintf(buf, "%10.6f", priv->gps_lon);
     draw_str(buf, 0, 9, ca, 0);
-
-#ifdef GPS_SAT_ICON
-    for (i = 0; i < 22; i++)
-        for (j = 0; j < 20; j++)
-            set_pixel(SAT_X+j, SAT_Y+i, sat_ico[i * 20 + j], ca);
-#endif
 
     sprintf(buf, "%2d", priv->gps_nrsats);
     draw_str(buf, SAT_X + 20 + 1, 0, ca, 0);
