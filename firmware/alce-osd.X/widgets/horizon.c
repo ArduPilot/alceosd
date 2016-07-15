f571ba55afe44ea101f5f278b9a623e9effd1217
Luis Alves <ljalvs@gmail.com>
2016-07-15 16:31:02 +0100

    widgets/horizon: Add new view mode
    Simple horizon with a compass around it.
    Also shows home location on the compass.

--- firmware/alce-osd.X/widgets/horizon.c
+++ firmware/alce-osd.X/widgets/horizon.c
@@ -22,7 +22,7 @@
 #define X_SHIFT 32
 
 #define X_SIZE  (192 - X_SHIFT)
-#define Y_SIZE  140
+#define Y_SIZE  154
 
 #define RANGE       Y_SIZE
 #define SCALE       5
@@ -41,6 +41,8 @@ struct widget_priv {
 
     int pitch_deg, roll_deg;
     float cos_roll, sin_roll;
+
+    int heading;
 };
 
 static void mav_callback(mavlink_message_t *msg, void *d)
@@ -59,6 +61,13 @@ static void mav_callback(mavlink_message_t *msg, void *d)
     schedule_widget(w);
 }
 
+static void mav_callback_hud(mavlink_message_t *msg, void *d)
+{
+    struct widget *w = d;
+    struct widget_priv *priv = w->priv;
+    priv->heading = mavlink_msg_vfr_hud_get_heading(msg);
+}
+
 static int open(struct widget *w)
 {
     struct widget_priv *priv;
@@ -74,12 +83,14 @@ static int open(struct widget *w)
     priv->sin_roll = sin(0);
     
     add_mavlink_callback(MAVLINK_MSG_ID_ATTITUDE, mav_callback, CALLBACK_WIDGET, w);
+    if (w->cfg->props.mode == 1)
+        add_mavlink_callback(MAVLINK_MSG_ID_VFR_HUD, mav_callback_hud, CALLBACK_WIDGET, w);
     w->ca.width = X_SIZE;
     w->ca.height = Y_SIZE;
     return 0;
 }
 
-static void render(struct widget *w)
+static void render_0(struct widget *w)
 {
     struct widget_priv *priv = w->priv;
     struct canvas *ca = &w->ca;
@@ -117,7 +128,6 @@ static void render(struct widget *w)
             x0 = cx + offset;
             offset = (int) size * priv->cos_roll;
             x1 = x0 + offset;
-
             
             offset = (int) gap * priv->sin_roll;
             y0 = cy + offset;
@@ -136,7 +146,6 @@ static void render(struct widget *w)
             offset = (int) size * priv->cos_roll;
             x1 = x0 - offset;
 
-
             offset = (int) gap * priv->sin_roll;
             y0 = cy - offset;
             offset = (int) size * priv->sin_roll;
@@ -152,10 +161,8 @@ static void render(struct widget *w)
         }
     }
 
-
     draw_ohline(X_CENTER - 3, X_CENTER + 3, Y_CENTER, 1, 3, ca);
     draw_vline(X_CENTER, Y_CENTER - 3, Y_CENTER + 3, 1, ca);
-
     
     float cos_i, sin_i;
 
@@ -211,20 +218,96 @@ static void render(struct widget *w)
     offset = (int) size * priv->cos_roll;
     x1 = x0 + offset;
 
-
     offset = (int) (gap-size) * priv->sin_roll;
     y0 = cy + offset;
     offset = (int) size * priv->sin_roll;
     y1 = y0 + offset;
-
 
     draw_line(x0-1, y0-1, x1+1, y1-1, 3, ca);
     draw_line(x0-1, y0+1, x1+1, y1+1, 3, ca);
     draw_line(x0, y0, x1, y1, 1, ca);
 }
 
+static void render_1(struct widget *w)
+{
+    struct widget_priv *priv = w->priv;
+    struct canvas *ca = &w->ca;
+    int x0, x1, y0, y1, offset, cx, cy, x, y, i;
+
+    cx = X_SIZE / 2;
+    cy = Y_CENTER;
+    
+    i = priv->pitch_deg ;
+
+    y = cy + (int) i * priv->cos_roll * 0.8;
+    x = cx - (int) i * priv->sin_roll * 0.8;
+
+    offset = (int) (X_SIZE/2 - 5) * priv->cos_roll;
+    x0 = x - offset; x1 = x + offset;
+    offset = (int) (X_SIZE/2 - 5) * priv->sin_roll;
+    y0 = y - offset; y1 = y + offset;
+    draw_oline(x0, y0, x1, y1, 1, ca);
+
+    i += 10;
+    
+    y = cy + (int) i * priv->cos_roll * 0.8;
+    x = cx - (int) i * priv->sin_roll * 0.8;
+
+    offset = (int) (X_SIZE/2 - 30) * priv->cos_roll;
+    x0 = x - offset; x1 = x + offset;
+    offset = (int) (X_SIZE/2 - 30) * priv->sin_roll;
+    y0 = y - offset; y1 = y + offset;
+    draw_oline(x0, y0, x1, y1, 1, ca);
+
+    /* compass */
+    for (i = 0; i < 89; i += 15) {
+        offset = i + priv->heading;
+        x = 70 * sin(DEG2RAD(offset));
+        y = 70 * cos(DEG2RAD(offset));
+        
+        if (i == 0) {
+            draw_jstr("S", cx + x, cy + y, JUST_VCENTER | JUST_HCENTER, ca, 2);
+            draw_jstr("E", cx + y, cy - x, JUST_VCENTER | JUST_HCENTER, ca, 2);
+            draw_jstr("N", cx - x, cy - y, JUST_VCENTER | JUST_HCENTER, ca, 2);
+            draw_jstr("W", cx - y, cy + x, JUST_VCENTER | JUST_HCENTER, ca, 2);
+        } else {
+            set_pixel(cx + x, cy + y, 1, ca);
+            set_pixel(cx - x, cy - y, 1, ca);
+            set_pixel(cx - y, cy + x, 1, ca);
+            set_pixel(cx + y, cy - x, 1, ca);
+            draw_circle(cx + x, cy + y, 2, 3, ca);
+            draw_circle(cx - x, cy - y, 2, 3, ca);
+            draw_circle(cx - y, cy + x, 2, 3, ca);
+            draw_circle(cx + y, cy - x, 2, 3, ca);
+        }
+    }
+
+    struct home_data *home = get_home_data();
+
+    offset = home->direction - 90;
+    x = 70 * cos(DEG2RAD(offset));
+    y = 70 * sin(DEG2RAD(offset));
+    draw_jstr("H", cx + x, cy + y, JUST_VCENTER | JUST_HCENTER, ca, 1);
+
+    draw_circle(cx, cy, 5, 1, ca);
+    draw_circle(cx, cy, 6, 3, ca);
+}
+
+void render(struct widget *w)
+{
+    switch (w->cfg->props.mode) {
+        default:
+        case 0:
+            render_0(w);
+            break;
+        case 1:
+            render_1(w);
+            break;
+    }
+}
+
 const struct widget_ops horizon_widget_ops = {
-    .name = "Articial Horizon",
+    .name = "Artificial Horizon",
     .mavname = "HORIZON",
     .id = WIDGET_HORIZON_ID,
     .init = NULL,
