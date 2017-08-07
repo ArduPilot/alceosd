d8f4e1f1a135798032a3d5c061b0c4bd0614c7f4
Luis Alves <ljalvs@gmail.com>
2017-08-07 22:29:25 +0100

    widget/throttle: Add more modes/sources
    Modes: vertical bar, horizontal bar, text only
    Sources added: controller, rc channel
    Units: raw, percentage

--- firmware/alce-osd.X/widgets/throttle.c
+++ firmware/alce-osd.X/widgets/throttle.c
@@ -22,17 +22,54 @@
 #define Y_SIZE  45
 
 struct widget_priv {
-    unsigned char throttle, last_throttle;
+    u16 throttle, last_throttle;
 };
 
 static void render_timer(struct timer *t, void *d)
 {
     struct widget *w = d;
     struct widget_priv *priv = w->priv;
-    mavlink_vfr_hud_t *vfr_hud = mavdata_get(MAVLINK_MSG_ID_VFR_HUD);
 
-    priv->throttle = (unsigned char) vfr_hud->throttle;
-    if (priv->throttle ==  priv->last_throttle)
+    switch (w->cfg->props.source) {
+        default:
+        case 0:
+        {
+            /* throttle setting from VFR_HUD (percent) */
+            mavlink_vfr_hud_t *vfr_hud = mavdata_get(MAVLINK_MSG_ID_VFR_HUD);
+            priv->throttle = (unsigned char) vfr_hud->throttle;
+            break;
+        }
+        case 1:
+        {
+            /* RC channel throttle stick */
+            /* param1 = rc channel nr */
+            mavlink_rc_channels_raw_t *rcr;
+            mavlink_rc_channels_t *rc;
+            u16 *val_ptr;
+            if (mavdata_age(MAVLINK_MSG_ID_RC_CHANNELS) < 5000) {
+                rc = mavdata_get(MAVLINK_MSG_ID_RC_CHANNELS);
+                val_ptr = &rc->chan1_raw;
+            } else {
+                rcr = mavdata_get(MAVLINK_MSG_ID_RC_CHANNELS_RAW);
+                val_ptr = &rcr->chan1_raw;
+            }
+            u16 ch = TRIM(w->cfg->params[0], 1, 18) - 1;
+            priv->throttle = *(val_ptr + ch);
+            break;
+        }
+    }
+
+    if ((w->cfg->props.units == 0) && (w->cfg->params[3] > 0)) {
+        /* percentage */
+        /* param3=min, param4=max */
+        u16 min = w->cfg->params[2];
+        u16 max = w->cfg->params[3];
+        u32 thr = TRIM(priv->throttle, min, max);
+        thr = ((thr - min) * 100) / (max - min);
+        priv->throttle = (u16)thr;
+    }
+
+    if (priv->throttle == priv->last_throttle)
         return;
 
     priv->last_throttle = priv->throttle;
@@ -50,8 +87,25 @@ static int open(struct widget *w)
     w->priv = priv;
 
     priv->last_throttle = 0xff;
-    w->ca.width = X_SIZE;
-    w->ca.height = Y_SIZE;
+    
+    switch (w->cfg->props.mode) {
+        default:
+        case 0:
+            /* 0-100% vertical bar */
+            w->ca.width = X_SIZE;
+            w->ca.height = Y_SIZE;
+            break;
+        case 1:
+            /* 0-100% horizontal bar */
+            w->ca.width = Y_SIZE;
+            w->ca.height = X_SIZE;
+            break;
+        case 2:
+            /* text only */
+            w->ca.width = (18*4);
+            w->ca.height = 16;
+            break;
+    }
     add_timer(TIMER_WIDGET, 250, render_timer, w);
     return 0;
 }
@@ -61,15 +115,33 @@ static void render(struct widget *w)
     struct widget_priv *priv = w->priv;
     struct canvas *ca = &w->ca;
     unsigned v = priv->throttle * (Y_SIZE-4) / 100;
-    char buf[5];
+    char buf[20];
     
-    draw_rect(0, 0, X_SIZE-1, Y_SIZE-1, 3, ca);
-    draw_rect(1, 1, X_SIZE-2, Y_SIZE-2, 1, ca);
-    if (priv->throttle > 0) {
-        draw_frect(2, Y_SIZE-3 - v, X_SIZE-3, Y_SIZE-3, 2, ca);
+    switch (w->cfg->props.mode) {
+        default:
+        case 0:
+            draw_rect(0, 0, X_SIZE-1, Y_SIZE-1, 3, ca);
+            draw_rect(1, 1, X_SIZE-2, Y_SIZE-2, 1, ca);
+            if (priv->throttle > 0) {
+                draw_frect(2, Y_SIZE-3 - v, X_SIZE-3, Y_SIZE-3, 2, ca);
+            }
+            snprintf(buf, 5, "%d", priv->throttle);
+            draw_jstr(buf, ca->width/2, ca->height/2, JUST_HCENTER|JUST_VCENTER, ca, 0);
+            break;
+        case 1:
+            draw_rect(0, 0, Y_SIZE-1, X_SIZE-1, 3, ca);
+            draw_rect(1, 1, Y_SIZE-2, X_SIZE-2, 1, ca);
+            if (priv->throttle > 0) {
+                draw_frect(2, 2, Y_SIZE-3 - v, X_SIZE-3, 2, ca);
+            }
+            snprintf(buf, 5, "%d", priv->throttle);
+            draw_jstr(buf, ca->width/2, ca->height/2, JUST_HCENTER|JUST_VCENTER, ca, 0);
+            break;
+        case 2:
+            snprintf(buf, 20, "Thr(%s) %d%c", (w->cfg->props.source == 0 ? "o" : "i"), priv->throttle, (w->cfg->props.units == 0 ? '%' : ' '));
+            draw_jstr(buf, ca->width/2, ca->height/2, JUST_HCENTER|JUST_VCENTER, ca, 0);
+            break;
     }
-    snprintf(buf, 5, "%d", priv->throttle);
-    draw_jstr(buf, X_SIZE/2, Y_SIZE/2, JUST_HCENTER|JUST_VCENTER, ca, 0);
 }
 
 
